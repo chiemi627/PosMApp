12a92c6922c3fe363b84ccc7d641800b77bc92f8
chiemi627 <chiemi@cs.tsukuba.ac.jp>
2015-06-04 17:23:20 +0900

    apiのurl変更

--- PosMApp/www/js/data.js
+++ PosMApp/www/js/data.js
@@ -21,7 +21,7 @@ var poster 			= [],
 
 // json ファイルの置き場所（URL, 仮）
 //var posMAppDataURL = "http://posmapp.tk/api/data.php";
-var posMAppDataURL = "http://localhost:63342/PosMApp/PosMApp/www/api/data_nosession.json";
+var posMAppDataURL = "http://localhost:63342/PosMApp_forked/PosMApp/www/api/data_nosession.json";
 
 function initData() {
 	// ポスターデータのJSONを置いておく（仮）
